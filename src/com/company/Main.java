d8c8973a0b5bd89001524f09220e719b14e51aa6
WayneJr <blairu92@gmail.com>
2021-07-19 19:57:44 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaskMenu newFile = TaskMenu.FILE;
+        TaskMenu newEdit = TaskMenu.EDIT;
+        TaskMenu newView = TaskMenu.VIEW;
+        System.out.println("The first Item of " + newFile + " is " + newFile.getFirstItem());
+        System.out.println("The first Item of " + newEdit + " is " + newEdit.getFirstItem());
+        System.out.println("The first Item of " + newView + " is " + newView.getFirstItem());
+
+        getNumbers();
+
+
+        // Create a project to model 5 mathematical
+        // constants.
+
+        // UML
+        // Model a school hierarchy.
+        // Person, Students, Principal, Teachers, Janitors.
+        // https://diagrams.net
+
+    }
+
+
+    public static void getNumbers(int... numbers) {
+
+        if (num == i) {
+            // perform some action.
+        }
+
+    }
+
+    // 10 mins
+    // Create an Enum to model a restaurant menu.
+    // They offer 3 categories of items. Appetizer, Main Course & Dessert.
+    // Appetizer - Steamed Prawns and cafe latte
+    // Main Course - Continental Rice and Deep Fried Chicken.
+    // Dessert - Cupcakes and Milk chocolate ice cream.
+    // Use your main method to out the result.
+}
